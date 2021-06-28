f9d665d64b3b5752ae7ef168072cf3d1a1b7adb6
fmedina25 <85526962+fmedina25@users.noreply.github.com>
2021-06-27 21:49:25 -0500

    def calculate present value function and present value of new loans function

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -125,10 +125,18 @@ new_loan = {
 #    The function should return the `present_value` for the loan.
 # YOUR CODE HERE!
 
+def calculate_present_value(future_value, remaining_months, annual_discount_rate):
+    pv = future_value / (1 + annual_discount_rate/12) **remaining_months
+    return pv
 
 # @TODO: Use the function to calculate the present value of the new loan given below.
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
 # YOUR CODE HERE!
+
+annual_discount_rate = .20
+
+present_value_of_new_loan = calculate_present_value(new_loan["future_value"], new_loan["remaining_months"], annual_discount_rate)
+
 print(f"The present value of the loan is: {present_value}")
 
 
